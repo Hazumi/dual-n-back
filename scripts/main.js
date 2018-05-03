41b66af3667d822fc6e3e5b6805ad8140472b3ab
Hazumi <shirohazumi@openmailbox.org>
2018-05-02 21:16:04 -0400

    added more stats

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
   const $1 = $('#1');
   const $2 = $('#2');
   const $3 = $('#3');
@@ -9,28 +8,44 @@ $(document).ready(function() {
   const $7 = $('#7');
   const $8 = $('#8');
   const $9 = $('#9');
-
   const $correct = $('#correct');
   const $incorrect = $('#incorrect');
-
-  var correct = 0;
-  var incorrect = 0;
-
+  const $totalPlacements = $('#total-placements');
+  const $missedPosition = $('#missed-position');
+  const $missedColor = $('#missed-color');
+  const $locationBtn = $('#location-btn');
+  const $colorBtn = $('#color-btn');
   const posArr = [$1, $2, $3, $4, $5, $6, $7, $8, $9];
   const colorArr = ['red', 'blue', 'green', 'yellow', 'purple', 'black', 'pink', 'orange', 'gray', 'navy'];
+  let total = 0;
+  let correct = 0;
+  let incorrect = 0;
+  let missedPos = 0;
+  let missedColor = 0;
+  let square, color, rand9, rand10, lastColor1, lastColor2, lastColor3, lastPos1, lastPos2, lastPos3;
+  let positionClicked;
+  let colorClicked;
 
   const genRandNum = num => {
     return Math.floor(Math.random() * num );
   }
 
-  let square, color, rand9, rand10, lastColor1, lastColor2, lastPos1, lastPos2;
-
-  const $locationBtn = $('#location-btn');
-  const $colorBtn = $('#color-btn');
-
-  console.log(  );
-
   setInterval(function() {
+
+    if(square == lastPos3 && typeof lastPos3 !== 'undefined' && locationClicked === false) {
+      console.log('miss position');
+      missedPos++;
+      $missedPosition.text(missedPos);
+
+    }
+    if(color == lastColor3 && typeof lastColor3 !== 'undefined' && colorClicked === false) {
+      console.log('miss color');
+      missedColor++;
+      $missedColor.text(missedColor);
+    }
+
+    locationClicked = false;
+    colorClicked = false;
 
     if ($locationBtn.prop('disabled')) {
       $locationBtn.prop('disabled', false);
@@ -43,8 +58,11 @@ $(document).ready(function() {
       square.css('background-color', '#e8e8e8');
     }
 
+    lastColor3 = lastColor2;
     lastColor2 = lastColor1;
     lastColor1 = color;
+
+    lastPos3 = lastPos2;
     lastPos2 = lastPos1;
     lastPos1 = square;
 
@@ -53,6 +71,9 @@ $(document).ready(function() {
     square = posArr[rand9];
     color = colorArr[rand10]
     square.css('background-color', color);
+
+    total++;
+    $totalPlacements.text(total);
 
   }, 2500);
 
@@ -67,6 +88,7 @@ $(document).ready(function() {
   }
 
   $locationBtn.on('click', function() {
+    locationClicked = true;
     if(square == lastPos2 && typeof lastPos2 !== 'undefined') {
       updateCorrect();
     } else {
@@ -76,6 +98,7 @@ $(document).ready(function() {
   });
 
   $colorBtn.on('click', function() {
+    colorClicked = true;
     if(color == lastColor2 && typeof lastColor2 !== 'undefined') {
       updateCorrect();
     } else {
